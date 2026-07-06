9fb36598f33a0e6200906e9b71c92208d6e81942
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-07-05 19:51:11 -0500

    Crear WhatsApps.js

---
+++ WhatsApps.js
@@ -0,0 +1,30 @@
+🚀 ¡WHATSAPP PLUS GB OG Y PREMIUM YA DISPONIBLE!
+¿Cansado del WhatsApp normal? 🔥
+Descubre la versión que TODO el mundo está usando:
+✅ WhatsApp Plus GB OG
+✅ WhatsApp Plus Premium
+¿Qué te ofrece?
+✨ Privacidad Total
+Oculta tu última vez y en línea
+Lee mensajes sin que aparezca "visto"
+Bloquea chats con huella o PIN
+✨ Funciones Exclusivas
+Anti-eliminación (ver mensajes borrados)
+Descargar estados y fotos de perfil
+Más de 1000 temas y estilos
+Envía archivos hasta 1GB
+Copias de seguridad automáticas
+Modo avión mejorado y mucho más...
+✨ Diseño Premium
+Interfaz más bonita y moderna
+Emojis personalizados
+Fuentes y colores ilimitados
+¿Por qué miles ya lo tienen?
+Porque es rápido, seguro y sin límites.
+Precios especiales por tiempo limitado:
+WhatsApp Plus,GB,OG → $1 USD / pago único cualquier de ellos 1 dolar
+WhatsApp Plus Premium → $2 USD / pago único
+Combo OG + Premium → Precio especial (¡ahorra!)
+Instalación en 2 minutos
+Te ayudo paso a paso + soporte 24/7
+NUMERO:+51941658192
